4ce0fcd84fb111d9cdfb04a825916a546529a0aa
ryanhyun20 <ryan0304hyun@gmail.com>
2020-02-09 08:49:17 -0800

    testing midi integration and updating files on device

--- probability.py
+++ probability.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 import numpy as np
-from scipy.stats import cauchy
+from scipy.stats import cauchy, expon
 
 
 class PitchType(Enum):
@@ -9,7 +9,31 @@ class PitchType(Enum):
     SEMITONE = "semitone",
     WHOLETONE = "wholetone",
     OCTAVE = "octave",
-    OTHER = "other"
+    RESIDUAL = "residual"
+
+
+class MusicalEvent:
+    def __init__(self, prev_event, note_value, onset_time):
+        self.pitches = []       # List of MIDI degrees
+        self.residuals = prev_event.pitches
+        self.note_value = note_value
+        self.onset_time = onset_time
+
+    def get_C_i_chord(self):
+        return self.pitches
+
+    def get_C_i_semitone(self):
+        return set(note + 1 for note in self.pitches) + set(note - 1 for note in self.pitches)
+
+    def get_C_i_wholetone(self):
+        return set(note + 2 for note in self.pitches) + set(note - 2 for note in self.pitches)
+
+    def get_C_i_octave(self):
+        return set(note + 12 for note in self.pitches) + set(note - 12 for note in self.pitches)
+
+    def get_C_i_residual(self):
+        return self.residuals
+
 
 
 def emission_matrix(size_i = 10, size_s = 100, size_p = 10, size_t = 10):
@@ -30,35 +54,61 @@ def emission_matrix(size_i = 10, size_s = 100, size_p = 10, size_t = 10):
     return emission_matrix
 
 
-def pitch_probability(pitch, pitch_type, num_same_type):
+def pitch_probability(musical_event: MusicalEvent):
     """
-    :param pitch: MIDI value of the pitch
-    :param pitch_type: type of pitch (See PitchType Enum)
-    :param num_same_type: number of pitches of same pitch type as "pitch" in musical event i = "c_i^A
-    :return:
+    :param musical_event: the current musical event
+    :return: joint probability of all notes in musical event.
     """
 
     p_a = 0
-    if pitch_type is PitchType.CHORD:
-        p_a = 0.9497
-    elif pitch_type is PitchType.SEMITONE:
-        p_a = 0.0145
-    elif pitch_type is PitchType.WHOLETONE:
-        p_a = 0.0224
-    elif pitch_type is PitchType.OCTAVE:
-        p_a = 0.0047
+    joint_prob = 1
+
+    for pitch in musical_event.pitches:
+        pitch_type = pitch[1]
+        c_i_a = None
+
+        if pitch_type is PitchType.CHORD:
+            p_a = 0.9497
+            c_i_a = musical_event.get_C_i_chord()
+        elif pitch_type is PitchType.SEMITONE:
+            p_a = 0.0145
+            c_i_a = musical_event.get_C_i_semitone()
+        elif pitch_type is PitchType.WHOLETONE:
+            p_a = 0.0224
+            c_i_a = musical_event.get_C_i_wholetone()
+        elif pitch_type is PitchType.OCTAVE:
+            p_a = 0.0047
+            c_i_a = musical_event.get_C_i_octave()
+        elif pitch_type is PitchType.RESIDUAL:
+            p_a = 0.0086
+            c_i_a = musical_event.get_C_i_residual()
+
+        joint_prob *= p_a / len(c_i_a)  # maybe floating point errors.
+
+    return joint_prob
+
+def onset_time_probability(t_n, i_n, s_n, i_prev, s_prev, v, t_prev):
+    """
+    :param t_n: onset time of n-th note
+    :param i_n: nth musical event
+    :param s_n: number of notes played at nth musical event
+    :param i_prev: previous musical event
+    :param s_prev: number of notes played at previous musical event
+    :param v: tempo
+    :param t_prev: array of all previous onset times.
+    :return: Probability of onset time of nth note.
+    """
+
+    w1 = 0.5
+    w2 =  0.5
+    n = len(t_prev)     # maybe off by one
+
+    if s_n == 1:
+        return (w1 * P_IOI1(t_n - t_prev[i_prev.onset_time], i_n, i_prev, v, i_n.note_value)) + (w2 * P_IOI2(t_n - t_prev[n-1], i_n, i_prev, v, i_n.note_value))
     else:
-        p_a = 0.0086
+        return P_IOI3(t_n - t_prev[n-1], i_n, i_prev, v) if i_n == i_prev else 0
 
-
-    return p_a / num_same_type
-
-
-def onset_time_probability():
-
-    pass
-
-def P_IOI1(dt, i_n, i_n_minus_1, v, V_i):
+def P_IOI1(dt, i_n, i_prev, v, V_i):
     """
     Calculates the Inter-Onset Interval type 1
     :param dt: t_n - t_{n-s[n-1]} (see definition of IOI1)
@@ -68,12 +118,21 @@ def P_IOI1(dt, i_n, i_n_minus_1, v, V_i):
     :param V_i: note value (half, quarter, whole, etc...)
     :return: probability according to corresponding distribution
     """
-    if i_n == i_n_minus_1 + 1:
+
+    # Straight transition
+    if i_n == i_prev + 1:
+        return cauchy.pdf(dt, v * V_i, 0.4)
+    else:
         return cauchy.pdf(dt, v * V_i, 0.4)
 
 
-def P_IOI2(dt, i_n, i_n_minus_1, v):
-    pass
+def P_IOI2(dt, i_n, i_prev, v, V_i):
+    if i_n == i_prev + 1:
+        return cauchy.pdf(dt, (v * V_i) - dev, 0.4)
+        # dev is how much we missed the expected note length of event i
+        # the expected note length of event i is given by the number of notes times the average IOI of all those notes.
 
-def P_IOI3(dt, i_n, i_n_minus_1, v):
-    pass
+def P_IOI3(dt, pitch_type):
+
+    if pitch_type == PitchType.CHORD:
+        return expon.pdf(dt, 0, 0.0101)
