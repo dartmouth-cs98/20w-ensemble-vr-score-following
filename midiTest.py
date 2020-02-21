80f21e5ac55608d7fc811010db971d50ef73e1f0
ryanhyun20 <ryan0304hyun@gmail.com>
2020-02-21 14:22:29 -0800

    progress on using midi messages for playback

--- midiTest.py
+++ midiTest.py
@@ -1,17 +1,22 @@
 """
-Test code for MIDI file reading from the below repository.
-
-https://github.com/mido/mido/blob/stable/examples/midifiles/print_midi_file.py
+Test code for MIDI file reading.
 """
 import sys
+import mido
 from mido import MidiFile
+from midiTimeline import TimeLine
+
+
 
 if __name__ == '__main__':
     filename = sys.argv[1]
 
     midi_file = MidiFile(filename)
 
-    for i, track in enumerate(midi_file.tracks):
-        sys.stdout.write('=== Track {}\n'.format(i))
+    port = mido.open_output('New Port', virtual=True)
+    for track in midi_file.tracks:
         for message in track:
-            sys.stdout.write('  {!r}\n'.format(message))
+            port.send(message)
+"""    time_line = TimeLine(midi_file)
+    time_line.gen_time_line()
+    time_line.play_back()"""
