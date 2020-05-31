01f169f94bec5d8a0fa88eeb60850d7cbfde2ecc
MarshallPeng <marshall.s.peng.20@dartmouth.edu>
2020-05-31 02:42:59 -0500

    circleci

--- src/model/tempo.py
+++ src/model/tempo.py
@@ -1,3 +1,6 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 class KalmanFilter:
 
     def __init__(self, current_estimate, estimate_error=100 ** 2, measurement_error=10 ** 2, kalman_gain=None,
@@ -29,3 +32,18 @@ class KalmanFilter:
         self.create_estimate(measurement)
         self.update_error()
         return self.current_estimate
+
+if __name__ == "__main__":
+    kalman_filter = KalmanFilter(60)
+
+    true_tempos = [60, 65, 62, 63, 64, 65, 60, 65, 65, 66, 67, 68, 68, 68, 68, 68, 68, 68]
+    observed_tempos = [tempo + np.random.normal(0, 1) for tempo in true_tempos]
+    filter_values = [kalman_filter.next_measurement(tempo) for tempo in observed_tempos]
+
+    plt.plot(observed_tempos, color="green")
+    plt.plot(filter_values, color="red")
+    plt.xlabel("nth observation")
+    plt.ylabel("Tempo")
+    plt.legend(["observed tempos", "estimated tempo"])
+    plt.title("Tempo Adjustment Test")
+    plt.show()
